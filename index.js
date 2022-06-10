4cfcd05e695f5609c4231ec6927b33d394029e49
brob <bryanlrobinson@gmail.com>
2022-06-10 14:26:17 -0400

    Stubs out a couple functions

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ async function getAllData(url, collector = []) {
     })
     collector.push(...data.data);
     console.log(`done: `)
+    // TODO: UNCOMMENT when ready for bulk 
     // if (data.links.next) {
     //     await getAllData(data.links.next, collector);
     // }
@@ -29,13 +30,23 @@ async function getMembersWithLanguages(memberList) {
     return membersWithLanguage
 }
 
+function createMemberUpdateRequests(memberList) {
+    // TODO: create list of requests to send to Orbit for each member
+    // TODO: Use makeLanguageArray function to create an array of tags for each member
+}
 
+function makeLanguageArray(languages) {
+    return languages.map(language => `language:${language}`)
+}
 
 async function run() {
     const members = await getAllData('https://app.orbit.love/api/v1/bryan-personal/members?affiliation=member&start_date=2022-01-01&items=100')
 
     const membersWithLanguage = await getMembersWithLanguages(members)
-    console.log(membersWithLanguage.length)
+    // TODO: create list of requests to send to Orbit for each member
+    // The list should have an array of tags built from the array of languages for each member
+
+
 }
 
 
